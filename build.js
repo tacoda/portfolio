cc9bd6f2f7615fc221de1da86c49571b66e4cf77
Ian Johnson <tacoda@pm.me>
2018-12-21 00:16:32 -0500

    Add more partials, extract a layout, add a page for webit, add an error page, and add directory creation and file copying to build script

--- build.js
+++ build.js
@@ -2,6 +2,25 @@ var fs = require('fs');
 var sass = require('sass');
 var pug = require('pug');
 
+var dir = './public';
+
+if(!fs.existsSync(dir)){
+    fs.mkdirSync(dir);
+    console.log('public directory was created');
+}
+
+fs.copyFile('favicon.ico', 'public/favicon.ico', (err) => {
+  if (err) throw err;
+  console.log('favicon.ico was copied to public/favicon.ico');
+});
+
+var assetsDir = './public/assets';
+
+if(!fs.existsSync(assetsDir)){
+    fs.mkdirSync(assetsDir);
+    console.log('public assets directory was created');
+}
+
 sass.render({file: 'style.scss'}, function(error, result) {
   if(!error){
     // No errors during the compilation, write this result on the disk
@@ -15,6 +34,8 @@ sass.render({file: 'style.scss'}, function(error, result) {
     });
   }
 });
+
+// TODO: Iterate through templates since this is the same code
 
 var compileIndex = pug.compileFile('templates/index.pug');
 
@@ -32,12 +53,28 @@ pug.render(compileIndex(), {}, function(error, result) {
   }
 });
 
+var compileError = pug.compileFile('templates/error.pug');
+
+pug.render(compileError(), {}, function(error, result) {
+  if(!error){
+    // No errors during the compilation, write this result on the disk
+    fs.writeFile('public/error.html', result, function(err){
+      if(!err){
+        //file written on disk
+        console.log('file written to disk');
+      } else {
+        console.log(err);
+      }
+    });
+  }
+});
+
 var compileShotty = pug.compileFile('templates/shotty.pug');
 
 pug.render(compileShotty(), {}, function(error, result) {
   if(!error){
     // No errors during the compilation, write this result on the disk
-    fs.writeFile('public/projects/shotty.html', result, function(err){
+    fs.writeFile('public/shotty.html', result, function(err){
       if(!err){
         //file written on disk
         console.log('file written to disk');
